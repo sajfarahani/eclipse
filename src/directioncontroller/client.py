35bc85e13a06ea29d1203f5d7a625c612fbb2839
sajfrahani <sajjfarahani@outlook.com>
2018-11-17 14:33:27 +1100

    commit

--- src/directioncontroller/client.py
+++ src/directioncontroller/client.py
@@ -1,14 +1,40 @@
+import sys
 import socket
 
+TCP_IP = '127.0.0.1'
+TCP_PORT = 8192
+BUFFER_SIZE = 1024
+param = []
+i=0
 
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.bind((TCP_IP,TCP_PORT))
+#s.listen(1)
+
+#print 'Listening for client...'
+
+#conn, addr = s.accept()
+#print 'Connection address:', addr
 def client_socket():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b'Hello, world')
-        data = s.recv(8192)
-
-    print('Received', repr(data))
-
-
-# if __name__ == '__main__':
-#     client_socket()
+    while 1:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((TCP_IP,TCP_PORT))
+        s.listen(1)
+        while 1:
+            print 'Listening for client...'
+            conn, addr = s.accept()
+            print 'Connection address:', addr
+            data = conn.recv(BUFFER_SIZE)
+            if data == ";" :
+                conn.close()
+                print "Received all the data"
+                i=0
+                for x in param:
+                    print x
+                #break
+            elif data:
+                print "received data: ", data
+                param.insert(i,data)
+                i+=1
+                #print "End of transmission"
+        s.close()
