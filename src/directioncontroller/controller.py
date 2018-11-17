b39bdb7207bd75af706afc7becaa91f38ae4b12e
sajfrahani <sajjfarahani@outlook.com>
2018-11-17 13:59:00 +1100

    commit

--- src/directioncontroller/controller.py
+++ src/directioncontroller/controller.py
@@ -12,10 +12,10 @@ def json_message(direction):
     data = {}
     # data['instruction'] = direction
     local_ip = socket.gethostbyname(socket.gethostname())
-    data = [{
-        "sender" = local_ip,
-        "instruction" = direction
-    }]
+    data = {
+        'sender' = local_ip,
+        'instruction' = direction
+    }
 
     json_data = json.dumps(data, sort_keys=False, indent=2)
     print("data %s" % json_data)
