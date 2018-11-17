26cf8526f2a43b2896885509fe03a7d2418fa839
sajfrahani <sajjfarahani@outlook.com>
2018-11-17 13:32:07 +1100

    commit

--- src/directioncontroller/controller.py
+++ src/directioncontroller/controller.py
@@ -2,7 +2,7 @@ import json
 
 def json_message(direction):
     data = {}
-    data['instruction'] = 'direction'
+    data['instruction'] = direction
     json_data = json.dumps(data, sort_keys=False, indent=2)
     print("data %s" % json_data)
     return json_data
