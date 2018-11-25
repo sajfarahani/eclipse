183cd01deeadfe1553b5c000aa5f154b845632d6
sajfrahani <sajjfarahani@outlook.com>
2018-11-25 21:52:31 +1100

    commit

--- src/directioncontroller/controller.py
+++ src/directioncontroller/controller.py
@@ -1,35 +1,33 @@
 import json
 import socket
+from flask_restful import Resource, Api
 from flask import Flask
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8192         # The port used by the server
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+class Direction(Resource):
+    def get(self, direction):
+        local_ip = socket.gethostbyname(socket.gethostname())
+        data = {
+            'sender' : local_ip,
+            'instruction' : direction
+        }
 
-@app.route('/direction/start', methods = ['POST'])
-def post_direction():
-    local_ip = socket.gethostbyname(socket.gethostname())
-    data = {
-        'sender' : local_ip,
-        'instruction' : "start"
-    }
+        json_data = json.dumps(data, sort_keys=False, indent=2)
+        print("data %s" % json_data)
 
-    json_data = json.dumps(data, sort_keys=False, indent=2)
-    print("data %s" % json_data)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(json_data.encode())
+            data = s.recv(1024)
+            s.close()
+        print('Received', repr(data))
+        return "received post"
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(json_data.encode())
-        data = s.recv(1024)
-        s.close()
-    print('Received', repr(data))
-
-    return "received post"
-
+api.add_resource(Direction, '/direction/<direction>')
 
 if __name__ == '__main__':
     app.run(debug=True)
