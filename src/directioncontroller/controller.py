999544c0aff93657e49bdeae63e14d61fb37bef7
sajfrahani <sajjfarahani@outlook.com>
2018-11-25 21:22:45 +1100

    commit

--- src/directioncontroller/controller.py
+++ src/directioncontroller/controller.py
@@ -6,36 +6,29 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8192         # The port used by the server
 app = Flask(__name__)
 
-def json_message(direction):
+@app.route('/')
+def index():
+    return "Hello, World!"
 
+@app.route('/direction/start', methods = ['POST'])
+def post_direction():
     local_ip = socket.gethostbyname(socket.gethostname())
     data = {
         'sender' : local_ip,
-        'instruction' : direction
+        'instruction' : "start"
     }
 
     json_data = json.dumps(data, sort_keys=False, indent=2)
     print("data %s" % json_data)
 
-    post_message(json_data + ";")
-
-    return json_data
-
-
-def post_message(data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(data.encode())
+        s.sendall(json_data.encode())
         data = s.recv(1024)
+        s.close()
     print('Received', repr(data))
 
-
-@app.route('/')
-def index():
-    return "Hello, World!"
-
-@app.route('/direction', methods = ['POST'])
-def post_direction()
+    return "received post"
 
 
 if __name__ == '__main__':
